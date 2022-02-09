78154bbaa547d9120e6b77c257e9e502eaf9447c
willdodsonn <sodlax38@gmail.com>
2022-02-09 05:16:06 +0000

    Master JS

--- exercises/001-isOldEnoughToDrink/app.js
+++ exercises/001-isOldEnoughToDrink/app.js
@@ -1,3 +1,12 @@
 function isOldEnoughToDrink(){
     // your code here	
+    let legalAge = 21;
+    let base = 0;
+    if(legalAge < 21){
+        return "you aren't old enough yet"
+    }
+    else if(age >= 21){
+        return "drinks on you!"
+    }
 }
+console.log(isOldEnoughToDrink(11));
