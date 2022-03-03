799620995ddcad5d2078ddc9c9cf8e6ce93182d4
willdodsonn <sodlax38@gmail.com>
2022-03-03 13:19:42 +0000

    continued js practice

--- exercises/031-addFullNameProperty/app.js
+++ exercises/031-addFullNameProperty/app.js
@@ -1,3 +1,11 @@
 function addFullNameProperty(obj) {
   // tu codigo aqui
+  obj.fullName = obj.firstName + " " + obj.lastName;
+  return obj;
 }
+var person = {
+  firstName: 'Jade',
+  lastName: 'Smith'
+};
+addFullNameProperty(person);
+console.log(person.fullName);
--- exercises/033-isPersonOldEnoughToDrinkAndDrive/app.js
+++ exercises/033-isPersonOldEnoughToDrinkAndDrive/app.js
@@ -1,4 +1,14 @@
 function isPersonOldEnoughToDrinkAndDrive(person) {
   // the person object contains an "age" property inside
   // your code here
+  if(person.age >= 21){
+    return true
+  }
+
+  return false
 }
+// var obj = {
+//   age: 45
+// };
+// var output = isPersonOldEnoughToDrinkAndDrive(obj);
+// console.log(output);
